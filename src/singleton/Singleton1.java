6368e18b4b2f465c4d94e38c5b3b73722d83ee2b
wangzhiguo <17614430417@163.com>
2020-03-09 17:54:56 +0800

    singleton

--- src/singleton/Singleton1.java
+++ src/singleton/Singleton1.java
@@ -5,9 +5,27 @@ import singleton.instance.Instance;
 /**
  *
  * 饿汉式单例模式
+ * 不存在线程安全问题
  * @author wangzhiguo
  */
 public class Singleton1 {
 
+    /**
+     * 私有成员变量
+     */
+    private static final Instance INSTANCE = new Instance();
 
+    /**
+     * 公有的get方法
+     */
+    public Instance getInstance(){
+        return INSTANCE;
+    }
+
+    /**
+     * 私有的构造函数
+     */
+    private Singleton1() {
+
+    }
 }
