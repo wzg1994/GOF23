7fd7277fe2b453d5968b522b3fc3fce63378a73e
wangzhiguo <17614430417@163.com>
2020-03-09 17:58:39 +0800

    singleton

--- src/singleton/Singleton2.java
+++ src/singleton/Singleton2.java
@@ -1,8 +1,26 @@
 package singleton;
 
+import singleton.instance.Instance;
+
 /**
+ * 饿汉式单例
+ * 此单例利用静态代码块进行初始化
  * @author wangzhiguo
  */
 public class Singleton2 {
-    
+
+    private static Instance instance = null;
+
+    static {
+        instance = new Instance();
+    }
+
+    private Singleton2(){
+
+    }
+
+    public Instance getInstance(){
+        return instance;
+    }
+
 }
