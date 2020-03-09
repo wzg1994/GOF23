cfefa0d16216fc29c6c7168b7d60ce17872d5773
wangzhiguo <17614430417@163.com>
2020-03-09 18:01:49 +0800

    singleton

--- src/singleton/Singleton3.java
+++ src/singleton/Singleton3.java
@@ -1,8 +1,25 @@
 package singleton;
 
+import singleton.instance.Instance;
+
 /**
+ * 懒汉式单例（线程不安全）
  * @author wangzhiguo
  */
 public class Singleton3 {
-    
+
+    private Instance instance = null;
+
+    public Instance getInstance(){
+        //造成线程不安全的原因是这个if判断
+        if (instance == null) {
+            instance = new Instance();
+        }
+
+        return instance;
+    }
+
+    private Singleton3(){
+
+    }
 }
